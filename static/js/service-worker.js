e465819f52514d5103f2deefe1217c7b0e5dadd1
Redcharger <kwameadjei6780@gmail.com>
2025-10-29 15:44:55 +0000

    final working version

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,65 +1,28 @@
-const CACHE_NAME = 'redcharger-static-v1';
+const CACHE_NAME = 'redcharger-v1';
 const urlsToCache = [
   '/',
-  '/static/manifest.json',
+  '/static/js/manifest.json',
   '/static/icons/icon-192x192.png',
   '/static/icons/icon-512x512.png'
 ];
 
-// Install event
 self.addEventListener('install', (event) => {
   console.log('Service Worker: Installing');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
 });
 
-// Activate event
 self.addEventListener('activate', (event) => {
   console.log('Service Worker: Activated');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 
-// Fetch event
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        
-        return fetch(event.request).then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-          
-          return response;
-        });
-      })
+      .then((response) => response || fetch(event.request))
   );
 });
