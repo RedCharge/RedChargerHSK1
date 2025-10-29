d6312d3ce40840465b4f136d2e2fcd8bee0f1609
Redcharger <kwameadjei6780@gmail.com>
2025-10-29 12:21:30 +0000

    final working version

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
-self.addEventListener('install', e => {
-  console.log('Service worker installed');
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("hsk1-cache").then(cache => {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
